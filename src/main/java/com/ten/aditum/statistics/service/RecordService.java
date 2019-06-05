54a4c1b077f025033f83dc213a4db8e7fa2afea0
kevinten10 <596823919@qq.com>
2019-06-05 15:18:11 +0800

    更新

--- src/main/java/com/ten/aditum/statistics/service/RecordService.java
+++ src/main/java/com/ten/aditum/statistics/service/RecordService.java
@@ -25,6 +25,47 @@ public class RecordService {
         return recordDao.select(pojo);
     }
 
+    /**
+     * 获取数量
+     */
+    public int selectCount(Record pojo) {
+        return recordDao.selectCount(pojo);
+    }
+
+    /**
+     * 获取某段时间内的数量
+     */
+    public int selectCountBetweenDateTime(Record pojo, String startTime, String endTime) {
+        return recordDao.selectCountBetweenDateTime(pojo, startTime, endTime);
+    }
+
+    /**
+     * 获取某段时间内的数量
+     */
+    public int selectCountAfterDateTime(Record pojo, String startTime) {
+        return recordDao.selectCountAfterDateTime(pojo, startTime);
+    }
+
+    /**
+     * 获取此ID之后的数据
+     */
+    public List<Record> selectAfterTheId(Record pojo) {
+        if (pojo.getId() == null) {
+            return null;
+        }
+        return recordDao.selectAfterTheId(pojo);
+    }
+
+    /**
+     * 当前时间对应的时刻之后的数据
+     */
+    public List<Record> selectAfterTheDateTime(Record pojo) {
+        if (pojo.getVisiteTime() == null) {
+            return null;
+        }
+        return recordDao.selectAfterTheVisitTime(pojo);
+    }
+
     public int update(Record pojo) {
         return recordDao.update(pojo);
     }
